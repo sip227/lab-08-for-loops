c29b7b37df8f6b47bb7313e88d535b8a5eb790e3
FaithfulWater <faithfulwater@gmail.com>
2020-11-02 15:49:28 -0500

    updated largest.html, and dictionary.py over class demonstration.

--- dictionary.py
+++ dictionary.py
@@ -1,15 +1,15 @@
 myData = {"effective top tube length": 515, "seat tube length": 500, "seat tube angle": 74.4, "head tube angle": 70.5, "stack": 513, "reach": 367, "standover height": 755}
 
-keys = []
+myKeys = []
 
-values = []
+myValues = []
 
 
 for k, v in myData.items():
-	print("key:", k, ", value:", v)
-	keys.append(k), values.append(v)
+	print("key: ", str(k), ", value: ", str(v))
+	myKeys.append(k), myValues.append(v)
 	
 	
-print("ALL KEYS:", str(keys))
-print("ALL VALUES:", str(values))
+print("ALL KEYS: ", str(myKeys))
+print("ALL VALUES: ", str(myValues))
  
